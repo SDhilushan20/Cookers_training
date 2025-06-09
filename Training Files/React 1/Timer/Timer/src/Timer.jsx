de1e97cf0abbf21cf93e173d00d97370e4bdf5a2
SDhilushan20 <sdhilushan20@gmail.com>
2025-06-09 08:14:26 +0530

    Updated with all the training files covered until 09-06-2025

--- Training Files/React 1/Timer/Timer/src/Timer.jsx
+++ Training Files/React 1/Timer/Timer/src/Timer.jsx
@@ -5,16 +5,14 @@ function Timer() {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    // Start a timer that updates the seconds every 1000ms (1 second)
     const interval = setInterval(() => {
       setSeconds(prevSeconds => prevSeconds + 1);
     }, 1000);
 
-    // Cleanup function: clear the timer if component unmounts
     return () => {
       clearInterval(interval);
     };
-  }, []); // Empty dependency array = run only once (on mount)
+  }, []);
 
   return (
     <div className="timer">
